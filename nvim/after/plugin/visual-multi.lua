eec7874d5438e0090fa079d8d0c6cccf24a572b8
r0nsha <r0nsh4v1t@gmail.com>
2023-03-19 15:55:33 +0200

    foo

--- nvim/after/plugin/visual-multi.lua
+++ nvim/after/plugin/visual-multi.lua
@@ -1,15 +1,13 @@
-if is_windows() then
-	vim.keymap.set(
-		"n",
-		"<C-S-Up>",
-		"<CMD>call vm#commands#add_cursor_up(0, v:count1)<CR>",
-		{ noremap = true, silent = true }
-	)
+vim.keymap.set(
+	"n",
+	"<C-S-Up>",
+	"<CMD>call vm#commands#add_cursor_up(0, v:count1)<CR>",
+	{ noremap = true, silent = true }
+)
 
-	vim.keymap.set(
-		"n",
-		"<C-S-Down>",
-		"<CMD>call vm#commands#add_cursor_down(0, v:count1)<CR>",
-		{ noremap = true, silent = true }
-	)
-end
+vim.keymap.set(
+	"n",
+	"<C-S-Down>",
+	"<CMD>call vm#commands#add_cursor_down(0, v:count1)<CR>",
+	{ noremap = true, silent = true }
+)
