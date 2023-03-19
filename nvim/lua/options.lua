3a744cdef65ef9f3405f30751b54bfffca83136b
r0nsha <r0nsh4v1t@gmail.com>
2023-03-19 17:10:01 +0200

    wip

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -48,7 +48,18 @@ vim.opt.wrap = false
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 
--- neovide font
+-- Windows specific options
+if is_windows() then
+	vim.opt.shell = vim.fn.executable("powershell") and "powershell" or "pwsh"
+	vim.opt.shellcmdflag =
+		"-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+	vim.opt.shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait"
+	vim.opt.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+	vim.opt.shellquote = ""
+	vim.opt.shellxquote = ""
+end
+
+-- Neovide options
 vim.o.guifont = "NotoSansMono Nerd Font"
 -- vim.o.guifont = "Iosevka Nerd Font"
 -- vim.o.guifont = "Hack Nerd Font"
