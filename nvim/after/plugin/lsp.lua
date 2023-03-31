6e694f995f74339e16b606249e0d0889d1aab2f2
r0nsha <r0nsh4v1t@gmail.com>
2023-03-31 17:46:48 +0300

    move all lsp related things to lsp.lua

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -181,3 +181,21 @@ lsp.docker_compose_language_service.setup({
 })
 
 require("fidget").setup()
+
+local null_ls = require("null-ls")
+
+null_ls.setup({
+	on_attach = function(client, bufnr)
+		local bufopts = { noremap = true, silent = true, buffer = bufnr }
+
+		if client.server_capabilities.documentFormattingProvider then
+			vim.keymap.set("n", "<leader>f", function()
+				vim.lsp.buf.format({ async = true })
+			end, bufopts)
+		end
+	end,
+	sources = {
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.taplo,
+	},
+})
