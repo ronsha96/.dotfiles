b31182fbb3387ae708c2e781ad80da03096d7534
r0nsha <r0nsh4v1t@gmail.com>
2023-04-01 01:08:04 +0300

    wip

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -54,7 +54,7 @@ vim.opt.splitright = true
 if utils.is_windows() then
 	vim.opt.shell = vim.fn.executable("powershell") and "powershell" or "pwsh"
 	vim.opt.shellcmdflag =
-		"-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+	"-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
 	vim.opt.shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait"
 	vim.opt.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
 	vim.opt.shellquote = ""
