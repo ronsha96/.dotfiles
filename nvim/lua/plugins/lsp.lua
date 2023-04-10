38905a9ee672ab897ac7872f32f9ae88b0f779d6
r0nsha <r0nsh4v1t@gmail.com>
2023-04-10 23:34:24 +0300

    add solid snippets

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -251,7 +251,12 @@ return {
 
 			local luasnip = require("luasnip")
 
-			require("luasnip.loaders.from_vscode").lazy_load()
+			require("luasnip.loaders.from_vscode").lazy_load({
+				paths = {
+					"../../snippets/solid-snippets/snippets"
+				}
+			})
+
 			luasnip.filetype_extend("all", { "_" })
 
 			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
