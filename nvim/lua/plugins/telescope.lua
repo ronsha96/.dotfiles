75634c44a22701af637152195389fc7c0935aad1
r0nsha <r0nsh4v1t@gmail.com>
2023-04-19 14:59:22 +0300

    put prompt on top

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -32,7 +32,7 @@ return {
 					layout_strategy = "horizontal",
 					layout_config = {
 						horizontal = {
-							-- prompt_position = "top",
+							prompt_position = "top",
 							preview_width = 0.55,
 							results_width = 0.8,
 						},
