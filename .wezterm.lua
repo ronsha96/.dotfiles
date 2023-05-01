b8ae11b84af501a0451747e782ec738fb15c2032
r0nsha <r0nsh4v1t@gmail.com>
2023-05-01 20:15:26 +0300

    wip

--- .wezterm.lua
+++ .wezterm.lua
@@ -9,6 +9,16 @@ end
 
 config.force_reverse_video_cursor = true
 config.color_scheme = "Kanagawa (Gogh)"
+config.use_fancy_tab_bar = false
+
+config.tab_bar = {
+	font_size = 12,
+	active_tab = {
+		bold = true,
+	},
+}
+
+config.tab_bar_at_bottom = false
 
 -- config.font = wezterm.font("NotoSansMono Nerd Font")
 config.font = wezterm.font("Iosevka Nerd Font")
