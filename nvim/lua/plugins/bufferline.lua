4088b782c1d7ae8244470f1d4a306b63e93389b7
r0nsha <r0nsh4v1t@gmail.com>
2023-05-05 15:28:53 +0300

    fix bufferline & add test.lua

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -1,47 +1,54 @@
 return {
-	"akinsho/bufferline.nvim",
-	config = function()
-		require("bufferline").setup({
-			options = {
-				close_command = function(n)
-					require("mini.bufremove").delete(n, false)
-				end,
-				right_mouse_command = function(n)
-					require("mini.bufremove").delete(n, false)
-				end,
-				diagnostics = "nvim_lsp",
-				always_show_bufferline = false,
-				offsets = {
-					{
-						filetype = "neo-tree",
-						text = "Neo-tree",
-						highlight = "Directory",
-						text_align = "left",
+	{
+		"akinsho/bufferline.nvim",
+		config = function()
+			require("bufferline").setup({
+				options = {
+					close_command = function(n)
+						require("mini.bufremove").delete(n, false)
+					end,
+					right_mouse_command = function(n)
+						require("mini.bufremove").delete(n, false)
+					end,
+					diagnostics = "nvim_lsp",
+					always_show_bufferline = false,
+					offsets = {
+						{
+							filetype = "neo-tree",
+							text = "Neo-tree",
+							highlight = "Directory",
+							text_align = "left",
+						},
 					},
 				},
-			},
-		})
+			})
 
-		for i = 1, 9, 1 do
+			for i = 1, 9, 1 do
+				vim.keymap.set(
+					"n",
+					"<leader>b" .. i,
+					"<cmd>BufferLineGoToBuffer " .. i .. "<cr>",
+					{ desc = "Buffer: Go to 1" }
+				)
+			end
+
+			vim.keymap.set("n", "<leader>b,", "<cmd>BufferLineCyclePrev<cr>", { desc = "Buffer: Cycle prev" })
+			vim.keymap.set("n", "<leader>b.", "<cmd>BufferLineCycleNext<cr>", { desc = "Buffer: Cycle next" })
+
+			vim.keymap.set("n", "<leader>b<", "<cmd>BufferLineMovePrev<cr>", { desc = "Buffer: Move prev" })
+			vim.keymap.set("n", "<leader>b>", "<cmd>BufferLineMoveNext<cr>", { desc = "Buffer: Move next" })
+
+			vim.keymap.set("n", "<leader>bp", "<cmd>BufferLineTogglePin<cr>", { desc = "Buffer: Toggle pin" })
 			vim.keymap.set(
 				"n",
-				"<leader>b" .. i,
-				"<cmd>BufferLineGoToBuffer " .. i .. "<cr>",
-				{ desc = "Buffer: Go to 1" }
+				"<leader>bxu",
+				"<cmd>BufferLineGroupClose ungrouped<cr>",
+				{ desc = "Buffer: Close ungrouped" }
 			)
-		end
 
-		vim.keymap.set("n", "<leader>b,", "<cmd>BufferLineCyclePrev<cr>", { desc = "Buffer: Cycle prev" })
-		vim.keymap.set("n", "<leader>b.", "<cmd>BufferLineCycleNext<cr>", { desc = "Buffer: Cycle next" })
-
-		vim.keymap.set("n", "<leader>b<", "<cmd>BufferLineMovePrev<cr>", { desc = "Buffer: Move prev" })
-		vim.keymap.set("n", "<leader>b>", "<cmd>BufferLineMoveNext<cr>", { desc = "Buffer: Move next" })
-
-		vim.keymap.set("n", "<leader>bp", "<cmd>BufferLineTogglePin<cr>", { desc = "Buffer: Toggle pin" })
-		vim.keymap.set("n", "<leader>bxu", "<cmd>BufferLineGroupClose ungrouped<cr>", { desc = "Buffer: Close ungrouped" })
-
-		vim.keymap.set("n", "<leader>p", "<cmd>BufferLinePick<cr>", { desc = "Buffer: Pick" })
-	end,
+			vim.keymap.set("n", "<leader>p", "<cmd>BufferLinePick<cr>", { desc = "Buffer: Pick" })
+		end,
+	},
 	{
 		"echasnovski/mini.bufremove",
 		keys = {
