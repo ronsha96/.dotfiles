23720c4daa2cbdb7f31143e953a81442c2bbdb69
r0nsha <r0nsh4v1t@gmail.com>
2023-05-05 14:03:11 +0300

    wip

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -66,7 +66,7 @@ if utils.is_windows() then
 	vim.opt.shellxquote = ""
 end
 
-vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
-vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
-vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
-vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
+vim.fn.sign_define("DiagnosticSignError", { text = utils.icons.error .. " ", texthl = "DiagnosticSignError" })
+vim.fn.sign_define("DiagnosticSignWarn", { text = utils.icons.warning .. " ", texthl = "DiagnosticSignWarn" })
+vim.fn.sign_define("DiagnosticSignInfo", { text = utils.icons.info .. " ", texthl = "DiagnosticSignInfo" })
+vim.fn.sign_define("DiagnosticSignHint", { text = utils.icons.bulb, texthl = "DiagnosticSignHint" })
--- nvim/lua/utils.lua
+++ nvim/lua/utils.lua
@@ -4,4 +4,11 @@ function M.is_windows()
 	return (vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1) and vim.fn.has("wsl") == 0
 end
 
+M.icons = {
+	error = "",
+	warning = "",
+	info = "",
+	bulb = "",
+}
+
 return M
