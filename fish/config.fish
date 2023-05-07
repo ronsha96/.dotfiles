73ea4d589b5c2b0c9a738fe1e6135f64d664e244
ronsha <rons@connecteam.com>
2023-05-07 08:30:19 +0300

    add aliases

--- fish/config.fish
+++ fish/config.fish
@@ -72,10 +72,21 @@ fish_add_path $HOME/.local/bin
 # Node stuff
 set -x NVM_DIR "$HOME/.nvm"
 
-# Alias ls
-alias ll='exa -l --git --time-style=long-iso --group-directories-first'
-alias ls='exa --group-directories-first'
-alias tree='exa -T'
+# Alias ls to exa
+# general use
+alias ls='exa'															# ls
+alias l='exa -lbF --git'                                                # list, size, type, git
+alias ll='exa -lbGF --git'												# long list
+alias llm='exa -lbGd --git --sort=modified'								# long list, modified date sort
+alias la='exa -lbhHigUmuSa --time-style=long-iso --git --color-scale'	# all list
+alias lx='exa -lbhHigUmuSa@ --time-style=long-iso --git --color-scale'	# all + extended list
+
+# special views
+alias lS='exa -1'                                                       # one column, just names
+alias lt='exa --tree --level=2'                                         # tree
+
+# Alias zoxide
+alias cd='z'
 
 # Alias codelldb
 alias codelldb=$HOME/codelldb/extension/adapter/codelldb
